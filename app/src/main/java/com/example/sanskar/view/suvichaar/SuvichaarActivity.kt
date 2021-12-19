597063e78040f297c9d2c0b300421bf3d1c06f42
akash <akash.aher@mediassist.in>
2021-12-19 12:30:43 +0530

    added backgroundSheet for suvichaar

---
+++ app/src/main/java/com/example/sanskar/view/suvichaar/SuvichaarActivity.kt
@@ -0,0 +1,12 @@
+package com.example.sanskar.view.suvichaar
+
+import android.os.Bundle
+import com.example.sanskar.R
+import com.example.sanskar.view.PostLoginActivity
+
+class SuvichaarActivity() : PostLoginActivity() {
+
+    override fun setLayoutId(): Int? {
+        return R.layout.activity_suvichaar
+    }
+}
---
+++ common/src/main/java/com/example/common/view/customView/BackGroundSheet.kt
@@ -0,0 +1,47 @@
+package com.example.common.view.customView
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import androidx.cardview.widget.CardView
+import com.example.common.R
+import com.example.common.databinding.ViewBackgroundSheetBinding
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.shape.CornerFamily
+import com.google.android.material.shape.ShapeAppearanceModel
+import java.util.jar.Attributes
+
+class BackGroundSheet(context: Context, attributes: AttributeSet) :
+    MaterialCardView(context, attributes) {
+
+    private var binding: ViewBackgroundSheetBinding? = null
+
+    init {
+        initialiseView()
+        populateView()
+    }
+
+    private fun initialiseView() {
+        binding = ViewBackgroundSheetBinding.inflate(LayoutInflater.from(context))
+        addView(binding?.root)
+    }
+
+    private fun populateView() {
+        changeShape()
+        changeBackgroundView()
+    }
+
+    private fun changeBackgroundView() {
+
+    }
+
+    private fun changeShape() {
+        val shapeModel = ShapeAppearanceModel
+            .builder()
+            .setAllCorners(CornerFamily.ROUNDED, 80F)
+            .build()
+        shapeAppearanceModel = shapeModel
+    }
+
+}
